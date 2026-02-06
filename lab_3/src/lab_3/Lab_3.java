b37c5d8906713bd6d15d804d33f3a9669f0e4084
Clarissa Ramos <angie.ramos20137@gmail.com>
2026-02-06 08:41:14 -0600

    cccc

--- lab_3/src/lab_3/Lab_3.java
+++ lab_3/src/lab_3/Lab_3.java
@@ -14,7 +14,6 @@ public class Lab_3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
